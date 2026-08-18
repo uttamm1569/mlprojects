694922c303366603b9a017eaa3d4d12ef25d0d99
uttamm1569 <uttamm1569@gmail.com>
2026-08-18 13:28:17 +0530

    A

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -115,4 +115,4 @@ class DataTransformation:
             )   
         except Exception as e:
             raise CustomException(e,sys)
-                
+         
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+import pickle
 
 def save_object(file_path,obj):
     try:
@@ -38,3 +39,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
